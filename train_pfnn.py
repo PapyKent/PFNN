8d61305bed37b08fe64d0c0a004b8de83d646eb8
PapyKent <kentdu38@gmail.com>
2017-12-13 12:50:36 +0900

    mean/std + mask for injuries

--- train_pfnn.py
+++ train_pfnn.py
@@ -37,11 +37,12 @@ Xstd[w*1:w* 2] = Xstd[w*1:w* 2].mean() # Trajectory Future Positions
 Xstd[w*2:w* 3] = Xstd[w*2:w* 3].mean() # Trajectory Past Directions
 Xstd[w*3:w* 4] = Xstd[w*3:w* 4].mean() # Trajectory Future Directions
 Xstd[w*4:w*10] = Xstd[w*4:w*10].mean() # Trajectory Gait
+
 #mean injuries
 
 """ Mask Out Unused Joints in Input """
 
-#modify the weights, but how ?
+
 joint_weights = np.array([
     1,
     1e-10, 1, 1, 1, 1,
@@ -51,10 +52,21 @@ joint_weights = np.array([
     1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10,
     1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10]).repeat(3)
 
+
+mixamo_joint_weights = np.array([
+    1,
+    1, 1, 1e-10,
+    1, 1, 1e-10, 1e-10, 1e-10,
+    1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10,
+    1e-10, 1, 1, 1, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10,
+    1, 1, 1, 1,
+    1, 1, 1, 1]).repeat(3)
+
 Xstd[w*10+j*3*0:w*10+j*3*1] = Xstd[w*10+j*3*0:w*10+j*3*1].mean() / (joint_weights * 0.1) # Pos
 Xstd[w*10+j*3*1:w*10+j*3*2] = Xstd[w*10+j*3*1:w*10+j*3*2].mean() / (joint_weights * 0.1) # Vel
-Xstd[w*10+j*3*2:          ] = Xstd[w*10+j*3*2:          ].mean() # Terrain
-#mean injuries .mean() / 0.1 -> related to joints + modify terrain?
+Xstd[w*10+j*3*2:w*10+j*3*3] = Xstd[w*10+j*3*2:w*10+j*3*3].mean() / (mixamo_joint_weights * 0.1) # Inj
+Xstd[w*10+j*3*3:          ] = Xstd[w*10+j*3*3:          ].mean() # Terrain
+
 
 Ystd[0:2] = Ystd[0:2].mean() # Translational Velocity
 Ystd[2:3] = Ystd[2:3].mean() # Rotational Velocity
