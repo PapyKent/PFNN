3618ec38f68874d12e21e2c6d1c732498d9e8a17
ToraMAC <torauri@torauri.local>
2018-01-31 12:55:21 +0900

    phaseMaker modify

--- ToolBox/phaseMaker.py
+++ ToolBox/phaseMaker.py
@@ -1,13 +1,17 @@
 import numpy
 
 def main():
-    
+
     file_path = '../data/injuries/'
     file_name = 'Injured_Walk_RUL_extended_converted'
-    
+
     f = open(file_path+file_name+'_footsteps.txt')
+    ip = open(file_path+file_name+'_injuries.txt')
     p = open(file_path+file_name+'.phase','w') #output file name
     p = open(file_path+file_name+'.phase','a')
+    ilines = ip.readlines()
+    frames = ilines[0].split(":")
+    frame = int(frames[1])
     lines = f.readlines()
     i = 1;
     first = lines[0].split(" ")
@@ -18,8 +22,13 @@ def main():
     for line in lines:
         line = line.rstrip()
         data=line.split(" ")
-        if(len(data)!=2):
-            print("break")
+        if(len(data)==1):
+            start = i;
+            while(i<=int(data[0])):
+                n=float(i-start)/(float(int(data[0])-start)*2)+0.5
+                p.write("{0:.5f}".format(n)+'\n')
+                i=i+1
+
             break;
         if(i<int(data[0])):
             start = i
@@ -32,6 +41,10 @@ def main():
             n=(float(i)-float(data[0]))/((float(data[1])-float(data[0]))*2.0)
             p.write("{0:.5f}".format(n)+'\n')
             i=i+1
+
+    while(i<=frame):
+        p.writelines("0.00000\n")
+        i+=1
 
     f.close
     p.close
